b65722a9b4abcd9cc3a9a48a0e8cccaf46756b9c
allenwhite <awhite23@rocketmail.com>
2015-03-20 19:13:59 -0400

    first commit with basic opencart boobs

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.grosh.co/lotteryroot/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.grosh.co/lotteryroot/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/lotteryroot/catalog/');
+define('DIR_SYSTEM', '/var/www/html/lotteryroot/system/');
+define('DIR_LANGUAGE', '/var/www/html/lotteryroot/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/lotteryroot/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/lotteryroot/system/config/');
+define('DIR_IMAGE', '/var/www/html/lotteryroot/image/');
+define('DIR_CACHE', '/var/www/html/lotteryroot/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/lotteryroot/system/download/');
+define('DIR_UPLOAD', '/var/www/html/lotteryroot/system/upload/');
+define('DIR_MODIFICATION', '/var/www/html/lotteryroot/system/modification/');
+define('DIR_LOGS', '/var/www/html/lotteryroot/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'lottery');
+define('DB_PREFIX', 'oc_');
