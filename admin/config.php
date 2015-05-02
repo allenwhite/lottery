f183cd89f1194cc663f891526c00cf43edc1311a
Claudius Mbemba <mbembac@gmail.com>
2015-05-02 19:06:23 -0400

    fixing vagrant setup

--- admin/config.php
+++ admin/config.php
@@ -1,30 +1,30 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://www.grosh.co/lotteryroot/admin/');
-define('HTTP_CATALOG', 'http://www.grosh.co/lotteryroot/');
+define('HTTP_SERVER', 'http://www.lottery.dev/admin/');
+define('HTTP_CATALOG', 'http://www.lottery.dev/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://www.grosh.co/lotteryroot/admin/');
-define('HTTPS_CATALOG', 'http://www.grosh.co/lotteryroot/');
+define('HTTPS_SERVER', 'http://www.lottery.dev/admin/');
+define('HTTPS_CATALOG', 'http://www.lottery.dev/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/html/lotteryroot/admin/');
-define('DIR_SYSTEM', '/var/www/html/lotteryroot/system/');
-define('DIR_LANGUAGE', '/var/www/html/lotteryroot/admin/language/');
-define('DIR_TEMPLATE', '/var/www/html/lotteryroot/admin/view/template/');
-define('DIR_CONFIG', '/var/www/html/lotteryroot/system/config/');
-define('DIR_IMAGE', '/var/www/html/lotteryroot/image/');
-define('DIR_CACHE', '/var/www/html/lotteryroot/system/cache/');
-define('DIR_DOWNLOAD', '/var/www/html/lotteryroot/system/download/');
-define('DIR_UPLOAD', '/var/www/html/lotteryroot/system/upload/');
-define('DIR_LOGS', '/var/www/html/lotteryroot/system/logs/');
-define('DIR_MODIFICATION', '/var/www/html/lotteryroot/system/modification/');
-define('DIR_CATALOG', '/var/www/html/lotteryroot/catalog/');
+define('DIR_APPLICATION', '/var/www/admin/');
+define('DIR_SYSTEM', '/var/www/system/');
+define('DIR_LANGUAGE', '/var/www/admin/language/');
+define('DIR_TEMPLATE', '/var/www/admin/view/template/');
+define('DIR_CONFIG', '/var/www/system/config/');
+define('DIR_IMAGE', '/var/www/image/');
+define('DIR_CACHE', '/var/www/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/system/download/');
+define('DIR_UPLOAD', '/var/www/system/upload/');
+define('DIR_LOGS', '/var/www/system/logs/');
+define('DIR_MODIFICATION', '/var/www/system/modification/');
+define('DIR_CATALOG', '/var/www/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', 'vagrant');
+define('DB_PASSWORD', 'vagrant');
 define('DB_DATABASE', 'lottery');
 define('DB_PREFIX', 'oc_');
